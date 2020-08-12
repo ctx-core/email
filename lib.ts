0fd300342b0ced44d3e43053f05e2c81bd025c95
Brian Takita <brian.takita@gmail.com>
2020-08-11 21:16:35 -0700

    - @ctx-core/logger

--- lib.ts
+++ lib.ts
@@ -1,7 +1,4 @@
-import { log } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/email'
 export function _valid__email(email) {
-	log(`${logPrefix}|valid__email`)
 	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 	return re.test(email)
 }
