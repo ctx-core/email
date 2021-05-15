f741873557aa970943170ae5af3193b3a39a3376
Brian Takita <brian.takita@gmail.com>
2021-05-15 10:05:25 -0400

    ## 6.1.0
    ### Minor Changes
    
    - "noImplicitAny": true

--- src/_email_valid.d.ts
+++ src/_email_valid.d.ts
@@ -1,2 +1,2 @@
-export declare function _email_valid(email: any): boolean;
+export declare function _email_valid(email: string): boolean;
 export { _email_valid as _valid__email, _email_valid as validate_email, _email_valid as validate__email, };
